3161c9033fe573463fa3a15fc8f078db64fe42a9
Thomas.Dekeyser <thomas.dekeyser@gmail.com>
2015-05-25 18:37:53 +0200

    Upsmuk landing en login page

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -1,15 +1,15 @@
-@extends('app')
+@extends('pboapp')
 
 @section('content')
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Login via toernooinl credentials</div>
+				<div class="panel-heading">Gelieve eerst in te loggen met je loginnaam/wachtwoord van toernooi.nl</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<strong>Ai!</strong> Problemen met je login.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
@@ -18,25 +18,32 @@
 						</div>
 					@endif
 
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-6">
+                            <img src="https://toernooi.nl/images/logo_toernooi_nl.gif"/>
+
+                        </div>
+                    </div>
 					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Toernooinl login</label>
+							<label class="col-md-6 control-label">Club loginnaam ( @toernooi.nl) (ex. c30009)</label>
 							<div class="col-md-6">
 								<input type="username" class="form-control" name="username" value="{{ old('username') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Paswoord</label>
+							<label class="col-md-6 control-label">Wachtwoord</label>
 							<div class="col-md-6">
 								<input type="password" class="form-control" name="password">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
+							<div class="col-md-6 col-md-offset-6">
 								<div class="checkbox">
 									<label>
 										<input type="checkbox" name="remember"> Remember Me
@@ -46,7 +53,7 @@
 						</div>
 
 						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
+							<div class="col-md-6 col-md-offset-6">
 								<button type="submit" class="btn btn-primary">Login</button>
 
 							</div>
