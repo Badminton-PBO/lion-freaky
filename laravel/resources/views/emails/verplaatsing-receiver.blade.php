90b3f60b43308c8b716adbcdb3394cd4dd03acd8
Thomas.Dekeyser <thomas.dekeyser@gmail.com>
2015-06-02 22:42:05 +0200

    Verplaatsing: Include different email templates into correct flow.

---
+++ laravel/resources/views/emails/verplaatsing-receiver.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Verplaatsings aanvraag {{$hTeam}} - {{$oTeam}}</title>
+</head>
+<body>
+Beste,
+<br/><br/>
+
+{{$requester}} diende een verzoek in om de wedstrijd "{{$hTeam}} - {{$oTeam}}" van "{{$dateTimeLayout}}" te verplaatsen.<br/>
+U kan de data bekijken en bevestigen of een nieuw tegenvoorstel indienen via <a href="{!! $link !!}">{!! $link !!}</a>
+<br><br>
+
+Met vriendelijk groeten,<br>
+<a href="http://www.badminton-pbo.be/">PBO</a>
+</body>
+</html>
---
+++ laravel/resources/views/emails/verplaatsing-requester.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Verplaatsings aanvraag {{$hTeam}} - {{$oTeam}}</title>
+</head>
+<body>
+Beste,
+<br/><br/>
+
+U diende net een verzoek in om de wedstrijd "{{$hTeam}} - {{$oTeam}}" van "{{$dateTimeLayout}}" te verplaatsen.<br/>
+U kan de data nog altijd corrigeren via <a href="{!! $link !!}">{!! $link !!}</a>
+<br><br>
+
+Met vriendelijk groeten,<br>
+<a href="http://www.badminton-pbo.be/">PBO</a>
+</body>
+</html>
