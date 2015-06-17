da7deb18e4f8e42a32bd373a30e5044a44442203
Thomas.Dekeyser <thomas.dekeyser@gmail.com>
2015-06-18 00:38:02 +0200

    Bugfix: receiver mail template

--- laravel/resources/views/emails/verplaatsing-receiver.blade.php
+++ laravel/resources/views/emails/verplaatsing-receiver.blade.php
@@ -8,6 +8,6 @@
     Beste,
     <br/><br/>
 
-    {{$requester}} heeft zonet wijzigingen aangebracht aan de verplaatsingsaanvraag voor de ontmoeting "{{$hTeam}} - {{$oTeam}}" van "{{$dateTimeLayout}}" te verplaatsen.<br/>
+    {{$requester}} heeft zonet wijzigingen aangebracht aan de verplaatsingsaanvraag voor de ontmoeting "{{$hTeam}} - {{$oTeam}}" van "{{$dateTimeLayout}}".<br/>
     Gelieve te bevestigen en/of een tegenvoorstel in te dienen via <a href="{!! $link !!}">{!! $link !!}</a>
 @endsection
