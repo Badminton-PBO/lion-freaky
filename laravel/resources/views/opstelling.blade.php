364f91d34df251931141554b3ade93f4fa7151ad
Thomas Dekeyser <thomas.dekeyser@gmail.com>
2016-09-04 18:16:54 +0200

    Opstellingsapp: fix incorrect c/p action coming from basisploegen app

--- laravel/resources/views/opstelling.blade.php
+++ laravel/resources/views/opstelling.blade.php
@@ -335,7 +335,7 @@
                                 </td>
                                 <td>
                                     <div>
-                                        <span data-bind="text: totalFixedIndexInsideTeamLayout"></span>
+                                        <span data-bind="text: totalIndexWithLayout"></span>
                                     </div>
                                 </td>
                             </tr>
