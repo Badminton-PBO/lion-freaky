baa42547ef19a27e5977880531243bd63a56777e
ThomasDekeyser <thomas.dekeyser@gmail.com>
2021-08-21 20:27:12 +0200

    Gebruik tijdelijk (een deel van) de basisopstellingen van vorig seizoen bij gebrek aan data....

--- laravel-5.7.0/app/Http/Controllers/DBLoadController.php
+++ laravel-5.7.0/app/Http/Controllers/DBLoadController.php
@@ -325,7 +325,7 @@ EOD;
                 DB::statement("set names latin1");//set to windows encoding
                 DB::statement("SET sql_mode = ''");// disable sql_mode=only_full_group_by as of mysql5.7, $insertLfTeamNamePrefix
                 //WARNING TMP disabling foreign key checks: ONLY USE THIS WHEN TESTING NEW DATA
-                DB::statement("SET FOREIGN_KEY_CHECKS=0");
+                //DB::statement("SET FOREIGN_KEY_CHECKS=0");
 
                 DBLoadController::loadCSV($clubCSV,'clubs');
                 DBLoadController::loadCSV($teamsCSV,'teams');
