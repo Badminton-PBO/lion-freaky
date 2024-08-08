50a6206d74507bdebc7d5f8ce34299ce1af71325
Thomas Dekeyser <thomas.dekeyser@noreply.com>
2024-08-08 18:25:36 +0200

    Update agenda's seizoen 24-25

--- laravel-9.x/resources/views/agenda.blade.php
+++ laravel-9.x/resources/views/agenda.blade.php
@@ -54,7 +54,7 @@
         $(document).ready(function(){
             $.ajax({
                 type: "GET",
-                url: "data/fixed/2023-2024/googleCalendarData.txt",
+                url: "data/fixed/2024-2025/googleCalendarData.txt",
                 dataType: "text",
                 success: function(data) {processData(data);}
             });
